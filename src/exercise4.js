d0c751a8b30e55898fdc1ace411baa942d5927e8
Killian Delorme <killiandelorme@MacBook-Pro-de-Killian.local>
2017-11-21 17:14:08 +0100

    Exercise 4

--- src/exercise4.js
+++ src/exercise4.js
@@ -3,6 +3,19 @@ exports.run = function(input) {
 
   // YOUR CODE IS WELCOME HERE
 
+	
+function repeat (NbArepeter, str)
+  {
+    var str_final = str;
+	var i = 1;
+	  while (i < NbArepeter) {
+		  str_final = str_final + ", " + str;
+		  i++;
+	  }
+    return (str_final);
+  }
+	
+	
   /** STRICTLY FORBIDDEN TO CHANGE THIS CODE */
   return input(3, "Beetlejuice", repeat);
   /** */
