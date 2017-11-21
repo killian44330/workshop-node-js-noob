d2a1ea206e8c0d78bf14beee65416a8861a13b19
Killian Delorme <killiandelorme@MacBook-Pro-de-Killian.local>
2017-11-21 16:51:14 +0100

    Exercise2

--- src/exercise2.js
+++ src/exercise2.js
@@ -1,3 +1,13 @@
 exports.run = function(input) {
-  console.log("Exercise #2");
+	console.log("Exercise #2");
+	var big_numb = 0;
+	var i = 0;
+	var tab = input.split("");
+	while (i < input.length) {
+		if (Number(tab[i]) > big_numb) {
+			big_numb = Number(tab[i]);
+			}		
+		i++;
+		}
+	return (big_numb);
 };
